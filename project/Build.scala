957a883b275a98ac97afb22ae1ef017d052232d4
Kumar Pandey <kumar.pandey@gmail.com>
2013-04-22 15:32:20 -0700

    db connection to mysql and changed no of connections to the pool

--- project/Build.scala
+++ project/Build.scala
@@ -10,9 +10,10 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+     // Add your project dependencies here
+    "mysql" % "mysql-connector-java" % "5.1.18"     
   )
-
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
